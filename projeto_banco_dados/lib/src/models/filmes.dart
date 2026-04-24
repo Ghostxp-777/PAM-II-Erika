cfc3fe2a3982d1cd8a976426858bd0ac9d17d9d0
Ghostxp <gustavopiacente6@gmail.com>
2026-04-24 11:37:27 -0300

    Add projeto_banco_dados, and configuration firebase.

---
+++ projeto_banco_dados/lib/src/models/filmes.dart
@@ -0,0 +1,45 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class Filme {
+  //final String id;
+  final String titulo;
+  final String genero;
+  final int ano;
+  final double nota;
+  final String descricao;
+
+  //construtor da classe
+  Filme({
+    required this.titulo,
+    required this.genero,
+    required this.ano,
+    required this.nota,
+    required this.descricao, 
+  });
+
+  Map<String, dynamic> topCreateMap(){
+    return{
+      'titulo': titulo,
+      'genero': genero,
+      'ano': ano,
+      'nota': nota,
+      'descricao': descricao,
+      'createAt': FieldValue.serverTimestamp(),
+    };
+  }
+
+  Map<String, dynamic> topUpdateMap(){
+    return{
+      'titulo': titulo,
+      'genero': genero,
+      'ano': ano,
+      'nota': nota,
+      'descricao': descricao,
+      'updateAt': FieldValue.serverTimestamp()
+    };
+  }
+  factory Filme.fromFirestore(DocumentSnapshot <Map<String, dynamic>> doc){
+    
+  }
+}
